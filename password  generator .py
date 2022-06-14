f88b6b4b6b1b94dc49e3215129bfbda0186a1b45
Urihel Coleman <45701213+Urihel@users.noreply.github.com>
2022-06-13 20:15:35 -0700

    Add files via upload

---
+++ password  generator .py
@@ -0,0 +1,10 @@
+#password generator
+import random
+import string
+
+entireAlphabet = string.ascii_letters
+print(entireAlphabet)
+lowerCaseAlphabet = entireAlphabet[0:26]
+print(lowerCaseAlphabet)
+upperCaseAlphabet = entireAlphabet[26: ]
+print(upperCaseAlphabet)
